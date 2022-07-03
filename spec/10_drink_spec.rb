7734689f629dce4e63a3c3a3dfb874adfc131c29
Aarya <amathrej@asu.edu>
2022-07-03 03:44:01 -0700

    Implement Solution for 10_drink_spec.rb

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -91,13 +91,15 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:my_boba) { described_class.new('boba', 16) }
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(my_boba.type).to eq('boba')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(my_boba).to be_full
       end
     end
   end
@@ -106,18 +108,20 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:my_large_boba) { described_class.new('boba', 24) }
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(my_large_boba).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:my_small_boba) { described_class.new('boba', 12) }
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(my_small_boba).not_to be_full
       end
     end
   end
