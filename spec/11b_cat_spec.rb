3da398f3fdf221578688a5ec6dd74251dbdac99c
Aarya <amathrej@asu.edu>
2022-07-04 03:53:27 -0700

    Implement solutions for 11b_cat_spec.rb

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:willow) { described_class.new('Willow', 'Tabby', 'Grey') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'Cat responds to talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(willow).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(willow).to be_hiding
   end
 end
