4ce6508b610f494e8e10355be2339cd547a430de
Aarya <amathrej@asu.edu>
2022-07-08 03:26:39 -0700

    Implement solution for Assignment 4 in 13_input_output_spec.rb

--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -176,9 +176,16 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when user inputs two incorrect values, then a valid input' do
       before do
+        incorrect_input_one = 'a'
+        incorrect_input_two = 'b'
+        correct_input = '3'
+
+        allow(game_loop).to receive(:player_input).and_return(incorrect_input_one, incorrect_input_two, correct_input)
       end
 
-      xit 'completes loop and displays error message twice' do
+      it 'completes loop and displays error message twice' do
+        expect(game_loop).to receive(:puts).with('Input error!').twice
+        game_loop.player_turn
       end
     end
   end
